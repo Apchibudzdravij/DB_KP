5e3aff9ec5f7efc695a150bf7e05dd900fe6144f
Apchibudzdravij <drevoteny@gmail.com>
2022-11-07 20:24:30 +0300

    welcome page complete, css added, welcome controller created

---
+++ src/main/java/com/belstu/drevoten/DB_KP/controller/WelcomeController.java
@@ -0,0 +1,26 @@
+package com.belstu.drevoten.DB_KP.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+    @Value("${welcome_en.message}")
+    private String message;
+
+    @Value("${error.message}")
+    private String errorMessage;
+
+    @GetMapping(value = {"/","/index", "/welcome"})
+    public ModelAndView index(Model model) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("welcome");
+        model.addAttribute("message", message);
+        model.addAttribute("errorMessage", errorMessage);
+
+        return modelAndView;
+    }
+}
